d572ebee71fb9b802d1b16a883aaf5c0cab37ae3
Prajjwal Goswami <mr.prajjwalji@gmail.com>
2023-03-25 23:42:49 +0530

    Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,6 +22,7 @@ const [active, setActive] = useState('')
           }}
           >
             <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
+            <p className='text-white text-[18px] font-bold cursor-pointer'>Prajjwal <span className='sm:block hidden'>| Front-End Developer </span></p>
           </Link>
 
       </div>
